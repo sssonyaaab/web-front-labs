ebcb181870ef68d5a196c52249f7215e47be0129
Соня Баранова <sonya.baranova999@gmail.com>
2024-05-26 14:28:36 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -1,6 +1,7 @@
 function showDate() {
     let out = document.getElementById('current-date');
     let today = new Date();
+    const weekDays = ['Воскресенье','Понедельник','Вторник','Среда','Четверг','Пятница','Суббота']
     out.innerHTML = `
         <p>Дата и время для русской локали: ${today.toLocaleString('ru-RU')}</p>
         <p>Дата и время для испанской локали: ${today.toLocaleString('es-ES')}</p>
@@ -8,6 +9,10 @@ function showDate() {
         <p>Дата и время для итальянской локали: ${today.toLocaleString('it-IT')}</p>
         <p>Дата и время для японской локали: ${today.toLocaleString('ja-JP')}</p>
         <p>Дата и время для турецкой локали: ${today.toLocaleString('tr-TR')}</p>
+        <p>Текущий год: ${today.getFullYear()}</p>
+        <p>Текущий месяц: ${today.getMonth()}</p>
+        <p>Текущая дата: ${today.getDay()}</p>
+        <p>День недели: ${weekDays[today.getDay()]}</p>
     `;
     out.style.color = 'black';
     out.style.margin = '10px';
